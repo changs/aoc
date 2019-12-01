d5e0ed3e70fa07e53e9ca64f4bb2e3fdd23fe685
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2019-12-01 15:48:42 +0100

    Second half of day 1 solved

--- test_aoc1.py
+++ test_aoc1.py
@@ -3,7 +3,7 @@ import io
 
 from aoc1 import fuel_requirement
 from aoc1 import calculate_sum
-
+from aoc1 import fuel_for_module
 
 class TestFuelRequirement(unittest.TestCase):
     def test_fuel_requirement(self):
@@ -12,9 +12,14 @@ class TestFuelRequirement(unittest.TestCase):
         self.assertEqual(fuel_requirement(1969), 654, "Should be 654")
         self.assertEqual(fuel_requirement(100756), 33583, "Should be 33583")
 
+    def test_fuel_for_module(self):
+        self.assertEqual(fuel_for_module(14), 2)
+        self.assertEqual(fuel_for_module(1969), 966)
+        self.assertEqual(fuel_for_module(100756), 50346)
+
     def test_calculate_sum(self):
-        stream = io.StringIO("12\n14\n1969\n100756\n")
-        self.assertEqual(calculate_sum(stream), 34241, "Should be 34241")
+        stream = io.StringIO("14\n1969\n100756\n")
+        self.assertEqual(calculate_sum(stream), 51314, "Should be 51314")
 
 if __name__ == '__main__':
     unittest.main()
