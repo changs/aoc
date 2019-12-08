46a1354a77342b89b674f6c931223be594f8f4f0
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2019-12-08 21:06:24 +0100

    Solution for the second part

--- aoc6.py
+++ aoc6.py
@@ -1,24 +1,51 @@
 import pdb
         
-def orbit_counter(stream):
+def load_data(stream):
     t = {}
     for node in iter(stream.splitlines()):
         p, n = node.split(')')
         t[n] = p
-        
+    return t
+
+def orbit_counter(t):
     orbits = 0
+    
     for i in range(len(t)):
         planet_key = list(t.keys())[-i]
         while planet_key != 'COM':
             orbits += 1
-            # print("Current: ", planet_key, " | Value: ", t[planet_key])
             planet_key = t[planet_key]
         
-    # print("Orbits: ", orbits)
-    # pdb.set_trace()
     return orbits
+
+def orbital_transfers(t):
+    distance = 0
+    planet_key_you = t['YOU']
+    planet_key_san = t['SAN']
+
+    path_you = []
+    path_you.append(planet_key_you)
+    while planet_key_you != 'COM':
+        path_you.append(t[planet_key_you])
+        planet_key_you = t[planet_key_you]
+    
+    path_san = []
+    path_san.append(planet_key_san)
+    while planet_key_san != 'COM':
+        path_san.append(t[planet_key_san])
+        planet_key_san = t[planet_key_san]
+
+    for i, el in enumerate(path_you):
+        if el in path_san:
+            dist_to_first_common_for_you = i
+            dist_to_first_common_for_san = path_san.index(el)
+            distance = dist_to_first_common_for_san + dist_to_first_common_for_you
+            break
+    return distance 
 
 if __name__ == "__main__":
     with open('./aoc6.in', 'r') as content_file:
         content = content_file.read()
-        print(orbit_counter(content))
+        t = load_data(content)
+        # print(orbit_counter(t))
+        print(orbital_transfers(t))
--- test_aoc6.py
+++ test_aoc6.py
@@ -15,7 +15,25 @@ D)I
 E)J
 J)K
 K)L"""
-        self.assertEqual(orbit_counter(example), 42)
+        t = load_data(example)
+        self.assertEqual(orbit_counter(t), 42)
+
+    def test_orbital_transfers(self):
+        example = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+        t = load_data(example)
+        self.assertEqual(orbital_transfers(t), 4)
 
 if __name__ == '__main__':
     unittest.main()
