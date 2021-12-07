9d18b9d6e6f00db293e6ded81850d8a48a9ebf0b
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-12-07 08:13:04 +0100

    small cleanup

--- 2021/aoc6.rb
+++ 2021/aoc6.rb
@@ -1,16 +1,9 @@
 require 'pry'
-
-# new fist = 8 days cycle
-# init fish = File cycle
-# if 0, reset counter to 6, create new fish with init 8
-# decrement all
-
 init = [3,4,3,1,2]
 
 #init = File.read('./aoc6_in.txt').split(',').map(&:to_i)
 
 #naive 
-#
 def cycle(init)
   new_fish = []
   init.each_index do |i|
@@ -26,15 +19,11 @@ end
 #256.times { |i| p i; p cycle(init).size }
 #puts init.size
 
-fish = Hash.new
-9.times { |i| fish[i] = 0 }
-init = File.read('./aoc6_in.txt').split(',').map(&:to_i)
-#init = [3,4,3,1,2]
-init.each { |f| fish[f] += 1 }
-
+fish = Hash.new(0)
+File.read('./aoc6_in.txt').split(',').map(&:to_i).each { |f| fish[f] += 1 }
 
 def cycleh(fish)
-  nf = Hash.new(0)
+  nf = {}
   nf[0] = fish[1]
   nf[1] = fish[2]
   nf[2] = fish[3]
@@ -46,7 +35,6 @@ def cycleh(fish)
   nf[8] = fish[0]
   nf
 end
-256.times { |i| p i; fish = cycleh(fish) }
+256.times { fish = cycleh(fish) }
 p fish.values.sum
-#256.times { |i| p i; cycle(init) }
 
