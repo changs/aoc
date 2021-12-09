fefb396e56dcbd4965eb47eb4ae6db4bea50852d
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2021-12-09 08:29:11 +0100

    part 2

--- 2021/aoc9.rb
+++ 2021/aoc9.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
-inp = File.read('./aoc9.in.txt')
+inp = File.read('./aoc9_in.txt')
 inp = inp.split.map(&:chars).map { |a| a.map(&:to_i) }
+
+basin_low = []
 
 r = inp.map.with_index do |h_row, y|
   h_row.map.with_index do |h, x|
@@ -9,8 +11,32 @@ r = inp.map.with_index do |h_row, y|
     next unless h < (inp[y] && inp[y][x + 1] || 10)
     next unless h < (inp[y - 1] && y - 1 >= 0 && inp[y - 1][x] || 10)
     next unless h < (inp[y + 1] && inp[y + 1][x] || 10)
+
+    basin_low.push [y, x]
     h
   end.reject(&:nil?)
 end
 risks = r.reject(&:nil?).flatten
 puts risks.sum + risks.size
+
+@inp = inp
+
+def neighbours(pos)
+  y, x = pos
+  n = []
+  v = @inp[y][x]
+  n.push([y, x + 1]) if (x + 1) < @inp[y].size && @inp[y][x + 1] != 9 && @inp[y][x + 1] > v
+  n.push([y + 1, x]) if (y + 1) < @inp.size && @inp[y + 1][x] != 9 && @inp[y + 1][x] > v
+  n.push([y - 1, x]) if (y - 1) >= 0 && @inp[y - 1][x] != 9 && @inp[y - 1][x] > v
+  n.push([y, x - 1]) if (x - 1) >= 0 && @inp[y][x - 1] != 9 && @inp[y][x - 1] > v
+  n
+end
+
+a = basin_low.map do |b|
+  n = [b]
+  n.each do |x|
+    n.concat(neighbours(x))
+  end
+  n
+end
+puts a.map { |b| b.uniq.size }.max(3).inject(1, :*)
